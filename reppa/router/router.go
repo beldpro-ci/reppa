0a25df67c8519c228dff7970e456413999ae910f
beldpro <beldpro@gmail.com>
2017-04-22 10:48:00 -0300

    Makes request logging a separate method

--- reppa/router/router.go
+++ reppa/router/router.go
@@ -11,16 +11,22 @@ import (
 
 var log = common.GetLogger()
 
-func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	log.Info("/",
+func logRequest(r *http.Request) {
+	log.Info("incoming request",
 		zap.String("user-agent", r.UserAgent()),
+		zap.String("host-header", r.Host),
+		zap.String("uri", r.RequestURI),
 		zap.String("method", r.Method),
 		zap.String("time", time.Now().Format("02/Jan/2006:15:04:05 -0700")))
+}
 
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	logRequest(r)
 	fmt.Fprint(w, "Welcome!\n")
 }
 
 func hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	logRequest(r)
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
